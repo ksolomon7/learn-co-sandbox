28cf33e87673633bd4d87c47a5adc7938967f5cf
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2021-05-05 20:48:14 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,14 @@
+# puts "Hello World!"
+
+# array=[1,2,3,4,5]
+# new_array=[]
+# length=array.length
+
+# array.length.times{ |index|
+#   new_array.push(array[index].to_s)
+# }
+
+# p new_array
+
+second_hash= Hash.new(:occupation=>"software engineer")
+puts second_hash
